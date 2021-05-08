f23f850faf67f273efc988ea2b6ee4e854dfd3ca
Ram Chavan <71763669+ram-123-g@users.noreply.github.com>
2021-05-08 23:02:11 +0530

    Assignment2

---
+++ Pattern13.java
@@ -0,0 +1,24 @@
+class Pattern13{
+
+public static void main(String[] args){
+
+for(char i='A';i<='E';i++){
+
+for(char j='D';j>=i;j--){
+
+System.out.print(" ");
+
+}
+for(char k='A';k<=i;k++){
+
+System.out.print(i+" ");
+
+}
+System.out.println();
+
+}
+
+
+}
+
+}
---
+++ Pattern9.java
@@ -0,0 +1,25 @@
+class Pattern9{
+
+public static void main(String[] args){
+
+for(char i='A';i<='E';i++){
+
+for(char j='D';j>=i;j--){
+
+System.out.print(" ");
+
+}
+for(char k='A';k<=i;k++){
+
+System.out.print(k+" ");
+
+
+}
+System.out.println();
+
+
+}
+
+}
+
+}
